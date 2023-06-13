3167290a5b035ab0e695d3391d563cb025aa3798
Flame <5017_30@mail.ru>
2023-06-13 22:17:08 +0300

    feat: add user card and user stat component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,26 +1,27 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { ReactComponent as SearchIcon } from 'assets/icon-search.svg'
+import { MyButton } from '../MyButton'
 
 import styles from './Search.module.scss'
-import { MyButton } from '../MyButton'
 
 interface SearchProps {
 	hasError: boolean
 	onSubmit: (text: string) => void
 }
 
+type FormFields = {
+	username: HTMLInputElement
+}
+
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-	const searchRef = useRef<HTMLInputElement | null>(null)
-
-	const handleSubmit = (event: React.FormEvent) => {
+	const handleSubmit = (
+		event: React.FormEvent<HTMLFormElement & FormFields>
+	) => {
 		event.preventDefault()
-		const text = searchRef.current?.value || ''
-
+		const text = event.currentTarget.username.value
 		if (text) {
 			onSubmit(text)
-			if (searchRef.current) {
-				searchRef.current.value = ''
-			}
+			event.currentTarget.reset()
 		}
 	}
 
@@ -34,7 +35,6 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
 					type='text'
 					className={styles.textField}
 					id='search'
-					ref={searchRef}
 					name='username'
 					placeholder='Search GitHub username...'
 				/>
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from '../types'
+
+export const defaultUser: LocalGithubUser = {
+	login: 'FlameFront-end',
+	id: 110346837,
+	avatar: 'https://avatars.githubusercontent.com/u/110346837?v=4',
+	name: 'Artem Kaliganov',
+	company: null,
+	blog: 'https://flamefront-end.github.io/kaliganov-frontend/',
+	location: 'Kaluga',
+	bio: null,
+	twitter: null,
+	repos: 31,
+	followers: 0,
+	following: 0,
+	created: '2022-07-31T16:14:13Z'
+}
