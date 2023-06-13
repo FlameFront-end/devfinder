02eab5492e2fda16c952b78726f1b2f250a9db27
Flame <5017_30@mail.ru>
2023-06-13 23:57:45 +0300

    feat: add user title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Container } from './components/Container'
-import { Header } from './components/Header'
-import { Search } from './components/Search'
-import { UserCard } from './components/UserCard'
-import { defaultUser } from './mock'
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { UserCard } from "./components/User/UserCard";
+import { defaultUser } from "./mock";
 
 function App() {
-	return (
-		<Container>
-			<Header />
-			<Search hasError onSubmit={() => {}} />
-			<UserCard {...defaultUser} />
-		</Container>
-	)
+  return (
+    <Container>
+      <Header />
+      <Search hasError onSubmit={() => {}} />
+      <UserCard {...defaultUser} />
+    </Container>
+  );
 }
 
-export default App
+export default App;
