1d1daafeb97be65bd912d54776ea899ad5ea15ac
Flame <5017_30@mail.ru>
2023-06-13 21:07:25 +0300

    Updates

--- src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.scss'
-import { ThemeSwitcher } from '../ThemeSwitcher'
-
-export const Header = () => (
-	<div className={styles.header}>
-		<div className={styles.logo}>devfinder</div>
-		<ThemeSwitcher />
-	</div>
-)
