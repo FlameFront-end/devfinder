6b43121483edda0dc5fdfbe7725248d6de3d11b5
Flame <5017_30@mail.ru>
2023-06-14 10:00:19 +0300

    feat: add search users

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,17 +1,16 @@
-import { LocalGithubUser } from '../types'
+import { LocalGithubUser } from "../types";
 
 export const defaultUser: LocalGithubUser = {
-	login: 'FlameFront-end',
-	id: 110346837,
-	avatar: 'https://avatars.githubusercontent.com/u/110346837?v=4',
-	name: 'Artem Kaliganov',
-	company: null,
-	blog: 'https://flamefront-end.github.io/kaliganov-frontend/',
-	location: 'Kaluga',
-	bio: null,
-	twitter: null,
-	repos: 31,
-	followers: 0,
-	following: 0,
-	created: '2022-07-31T16:14:13Z'
-}
+  login: "FlameFront-end",
+  avatar: "https://avatars.githubusercontent.com/u/110346837?v=4",
+  name: "Artem Kaliganov",
+  company: null,
+  blog: "https://flamefront-end.github.io/kaliganov-frontend/",
+  location: "Kaluga",
+  bio: "Junior Front-end developer",
+  twitter: null,
+  repos: 31,
+  followers: 0,
+  following: 0,
+  created: "2022-07-31T16:14:13Z",
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,36 +1,34 @@
 export type LocalGithubUser = {
-	login: string
-	id: number
-	avatar: string
-	name: string
-	company: string | null
-	blog: string | null
-	location: string | null
-	bio: string | null
-	twitter: string | null
-	repos: number
-	followers: number
-	following: number
-	created: string
-}
+  login: string;
+  avatar: string;
+  name: string;
+  company: string | null;
+  blog: string | null;
+  location: string | null;
+  bio: string | null;
+  twitter: string | null;
+  repos: number;
+  followers: number;
+  following: number;
+  created: string;
+};
 
 export type GithubUser = {
-	login: string
-	id: number
-	avatar_url: string
-	name: string
-	company: string | null
-	blog: string | null
-	location: string | null
-	bio: string | null
-	twitter_username: string | null
-	public_repos: number
-	followers: number
-	following: number
-	created_at: string
-}
+  login: string;
+  avatar_url: string;
+  name: string;
+  company: string | null;
+  blog: string | null;
+  location: string | null;
+  bio: string | null;
+  twitter_username: string | null;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+};
 
 export type GithubError = {
-	message: string
-	documentation_url: string
-}
+  message: string;
+  documentation_url: string;
+};
