18c1165cf54fa1d04a0bf5c7085d48e47602cb43
Flame <5017_30@mail.ru>
2023-06-14 09:08:25 +0300

    feat: add info item and user info component

--- src/components/User/UserCard/UserCard.tsx
+++ src/components/User/UserCard/UserCard.tsx
@@ -3,6 +3,7 @@ import { LocalGithubUser } from "../../../types";
 
 import styles from "./UserCard.module.scss";
 import { UserTitle } from "../UserTitle";
+import { UserInfo } from "../UserInfo";
 
 interface UserCardProps extends LocalGithubUser {}
 
@@ -22,6 +23,12 @@ export const UserCard = (props: UserCardProps) => {
         repos={props.repos}
         followers={props.followers}
         following={props.following}
+      />
+      <UserInfo
+        company={props.company}
+        blog={props.blog}
+        location={props.location}
+        twitter={props.twitter}
       />
     </div>
   );
